3e2ad3d3a048c3ef50749e6cafa74aa22dd1673f
mrobertsCSG <mroberts@csg.org>
2023-05-05 11:11:50 -0400

    add tabsetpanel

--- prep/dataframes.R
+++ prep/dataframes.R
@@ -1,32 +1,34 @@
 
 
 # State Donuts
-load(file = "data/all_donut_currently_eligible.rds")
-load(file = "data/all_donut_future_eligible.rds")
+load(file = paste0(sp_data_path, "/data/analysis/all_donut_currently_eligible.rds"))
+load(file = paste0(sp_data_path, "/data/analysis/all_donut_future_eligible.rds"))
 
+# load packages
+source("prep/00_library.R")
 
 # Parole Overview
-load(file = "data/people_released_to_parole_race.rds")
-load(file = "data/people_released_to_parole_sex.rds")
-load(file = "data/people_released_to_parole_age.rds")
-load(file = "data/people_released_to_parole_age_median.rds")
-load(file = "data/people_released_to_parole_education_median.rds")
+load(file = paste0(sp_data_path, "/data/analysis/people_released_to_parole_race.rds"))
+load(file = paste0(sp_data_path, "/data/analysis/people_released_to_parole_sex.rds"))
+load(file = paste0(sp_data_path, "/data/analysis/people_released_to_parole_age.rds"))
+load(file = paste0(sp_data_path, "/data/analysis/people_released_to_parole_age_median.rds"))
+load(file = paste0(sp_data_path, "/data/analysis/people_released_to_parole_education_median.rds"))
 
 
 # Parole Eligibility
-load(file = "data/all_pie_parole_elgibility_offense.rds")
-load(file = "data/parole_eligibility_table_2020.rds")
-load(file = "data/current_ped_2020_offenses.rds")
+load(file = paste0(sp_data_path, "/data/analysis/all_pie_parole_elgibility_offense.rds"))
+load(file = paste0(sp_data_path, "/data/analysis/parole_eligibility_table_2020.rds"))
+load(file = paste0(sp_data_path, "/data/analysis/current_ped_2020_offenses.rds"))
 
 
 # Releases from Prison
-load(file = "data/all_pie_released_at_ped.rds")
-load(file = "data/all_line_pop_released_to_parole.rds")
+load(file = paste0(sp_data_path, "/data/analysis/all_pie_released_at_ped.rds"))
+load(file = paste0(sp_data_path, "/data/analysis/all_line_pop_released_to_parole.rds"))
 
 
 # RRI
-load(file = "data/race_eth_rri_table.rds")
+load(file = paste0(sp_data_path, "/data/analysis/race_eth_rri_table.rds"))
 
 
 # Predicted Probabilities
-load(file = "data/all_pp_by_variable.rds")
+load(file = paste0(sp_data_path, "/data/analysis/all_pp_by_variable.rds"))
